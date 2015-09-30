62b1ddd9cd4b311218792ed1a619e8183ded7e3d
David van Leeuwen <david@madeawkward.com>
2015-09-30 15:12:37 +0200

    Log output

--- simple-dyno.js
+++ simple-dyno.js
@@ -1,5 +1,4 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.simpleDyno = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (__dirname){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -51,11 +50,11 @@ function setConfig() {
   // };
 
   if (_debug2['default'].local) {
-    (0, _child_process.exec)("sudo killall java", { cwd: __dirname }, function (err, stdout, stderr) {
-      localDynamo.launch(null, 4567);
-      options.endpoint = new AWS.Endpoint('http://localhost:4567');
-      logger("Started local DynamoDB on http://localhost:4567");
-    });
+    var stdout = (0, _child_process.execSync)("sudo killall java");
+    logger('killall java: ' + stdout.split('\n').join(''));
+    localDynamo.launch(null, 4567);
+    options.endpoint = new AWS.Endpoint('http://localhost:4567');
+    logger("Started local DynamoDB on http://localhost:4567");
   }
 
   exports.client = client = new AWS.DynamoDB(options);
@@ -108,7 +107,6 @@ function setTable(name, hashKey, rangeKey) {
     // });
   });
 }
-}).call(this,"/build")
 },{"./debug":2,"aws-sdk":undefined,"child_process":undefined,"dynamodb-doc":undefined,"local-dynamo":undefined}],2:[function(require,module,exports){
 "use strict";
 
