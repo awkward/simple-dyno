99f184e28ef2732c23ae028245f3b8b835f9c0d8
David van Leeuwen <david@madeawkward.com>
2015-11-28 14:07:41 +0100

    Add inMemory option

--- simple-dyno.js
+++ simple-dyno.js
@@ -1,4 +1,5 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.simpleDyno = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (__dirname){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -37,15 +38,16 @@ var doc = undefined;
 exports.doc = doc;
 
 function setConfig() {
-  var options = arguments.length <= 0 || arguments[0] === undefined ? { accessKeyId: "", secretAccessKey: "", region: "eu-west-1", runLocal: true } : arguments[0];
+  var options = arguments.length <= 0 || arguments[0] === undefined ? { accessKeyId: "", secretAccessKey: "", region: "eu-west-1", runLocal: true, inMemory: true } : arguments[0];
 
   if ((_debug2['default'].local || process.env.NODE_ENV === 'test') && options.runLocal) {
     try {
       var stdout = (0, _child_process.execSync)("killall java");
       logger('killall java: ' + stdout.split('\n').join(''));
     } catch (e) {}
-
-    localDynamo.launch(null, 4567);
+    var dbDir = null;
+    if (!inMemory) dbDir = __dirname + '/database';
+    localDynamo.launch(dbDir, 4567);
     options.endpoint = new AWS.Endpoint('http://localhost:4567');
     logger("Started local DynamoDB on http://localhost:4567");
   } else if (_debug2['default'].local || process.env.NODE_ENV === 'test') {
@@ -115,6 +117,7 @@ function setTable(name, hashKey, rangeKey) {
   });
 }
 
+}).call(this,"/lib")
 },{"./debug":2,"aws-sdk":undefined,"child_process":undefined,"local-dynamo":undefined}],2:[function(require,module,exports){
 "use strict";
 
