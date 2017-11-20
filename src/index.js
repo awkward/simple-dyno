9395ab81377f26e8f0a21cc247f9d07e8f4329f9
David van Leeuwen <david@madeawkward.com>
2017-11-20 17:28:00 +0100

    Fix finding package.json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { version } from '../package';
+import { version } from '../package.json';
 export { version };
 export { config, local, load, client, doc } from './db';
 export { Model } from './model';
